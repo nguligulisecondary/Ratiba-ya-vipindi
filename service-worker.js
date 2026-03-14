4202731044f63cb5f9854f49d7fa1cc9693987a3
nguligulisecondary <barakajohnuo4488@gmail.com>
2026-03-14 11:23:00 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,20 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker Installed");
+const CACHE_NAME = 'ratiba-vipindi-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
